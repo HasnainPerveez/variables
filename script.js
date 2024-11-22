21bb5c2d20d1324669054bb391bb9f2b181ea2e5
Hasnain Perveez <hasnainansari22441@gmail.com>
2024-11-22 11:25:21 +0500

    variables

---
+++ script.js
@@ -0,0 +1,6 @@
+var firstName = "Hasnain", fatherName = "PerveezAlam", myAge = 20;
+console.log("My name is ".concat(firstName, ",My father name  is ").concat(fatherName, ", I am ").concat(myAge, " years old"));
+alert(firstName + fatherName + myAge);
+console.log(firstName);
+console.log(fatherName);
+console.log(myAge);
---
+++ script.ts
@@ -0,0 +1,7 @@
+let firstName = "Hasnain", fatherName = "PerveezAlam", myAge = 20;
+console.log(`My name is ${firstName},My father name  is ${fatherName}, I am ${myAge} years old`);
+alert(firstName+fatherName+myAge);
+
+console.log(firstName);
+console.log(fatherName);
+console.log(myAge);
